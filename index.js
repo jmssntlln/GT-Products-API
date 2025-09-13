bce2ceb3a7684d962e65ce7d1fcc4036bb9aea97
James Santillan <james@sntllnjames-MacBook-Pro.local>
2025-09-13 13:58:44 +0800

    Initial work for ENV branch

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ import express from 'express';
 import postRoutes from './src/routes/post.routes.js';
 import commentRoutes from './src/routes/comment.routes.js';
 
+dotenv.config();
+
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
